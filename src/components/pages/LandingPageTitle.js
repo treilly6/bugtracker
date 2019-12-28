9baf9c1acde3a38963b6fcb896fc34d5ae736e71
treilly6 <treilly6@binghamton.edu>
2019-12-27 20:25:03 -0500

    text fade on landing page finished

--- src/components/pages/LandingPageTitle.js
+++ src/components/pages/LandingPageTitle.js
@@ -3,21 +3,44 @@ import '../../App.css';
 
 class LandingPageTitle extends React.Component {
     state={
-        opacity : 0,
+        opacity1 : 0,
+        opacity2 : 0,
+        opacity3 : 0,
     }
 
-    textHandler = async () => {
+    textHandler = () => {
         console.log("IN THE TEXT HANDLER");
-        await setTimeout(() => {
+        setTimeout(() => {
             console.log("IN THE SET TIMEOUT");
-            this.setState({opacity : 1});
-        }, 250);
 
-        setInterval(() => {
-            console.log("IN THE INTERVAL");
-            var value = (this.state.opacity === 0 ? 1 : 0);
-            this.setState({opacity : value});
-        }, 5000)
+            setTimeout(() => {
+                this.setState({opacity1 : 1});
+            }, 1500);
+
+            setTimeout(() => {
+                this.setState({opacity2 : 1});
+            }, 3000);
+
+            setTimeout(() => {
+                this.setState({opacity3 : 1});
+            }, 4500);
+
+            setInterval(() => {
+                console.log("TIME VAR")
+                this.setState({opacity1 : 0, opacity2 : 0, opacity3 : 0});
+                setTimeout(() => {
+                    this.setState({opacity1 : 1});
+                }, 1500);
+
+                setTimeout(() => {
+                    this.setState({opacity2 : 1});
+                }, 3000);
+
+                setTimeout(() => {
+                    this.setState({opacity3 : 1});
+                }, 4500);
+            }, 8000);
+        }, 250);
     }
 
     componentDidMount() {
@@ -27,9 +50,11 @@ class LandingPageTitle extends React.Component {
 
     render() {
         return(
-            <React.Fragment>
-                <h1 className="titleText" style={{opacity : this.state.opacity}}>Centralized Project Management</h1>
-            </React.Fragment>
+            <div className="titleText">
+                <h1 style={{opacity : this.state.opacity1, transition : "1s", fontSize : "2em"}}>Centralized</h1>
+                <h1 style={{opacity : this.state.opacity2, transition : "1s", fontSize : "2em"}}>Project</h1>
+                <h1 style={{opacity : this.state.opacity3, transition : "1s", fontSize : "2em"}}>Management</h1>
+            </div>
         )
     }
 }
