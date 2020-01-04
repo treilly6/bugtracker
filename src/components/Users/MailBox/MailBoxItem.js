290bf52137f5acc1226903a7d33e55e897d7621f
treilly6 <treilly6@binghamton.edu>
2020-01-03 23:54:42 -0500

    mouse event working for mail

--- src/components/Users/MailBox/MailBoxItem.js
+++ src/components/Users/MailBox/MailBoxItem.js
@@ -30,18 +30,19 @@ class MailBoxItem extends React.Component {
 
 
         return (
-            <div className="mailItemDiv" onMouseEnter={() => this.handleHover} onMouseLeave={() => this.handleHover}>
+            <div className="mailItemDiv" onMouseEnter={this.handleHover} onMouseLeave={this.handleHover}>
                 <Link to={`/mail/${mail._id}`}>
                     <div style={{display : "flex", justifyContent : "space-between", flexWrap : "wrap"}} onClick={this.handleMailHeight}>
                         <div>{mail.title}</div>
                         <div>
-                            <span style={{padding : "0px 3px", display : (this.state.hover ? "none" : "block"),}}>{date}</span><span style={{padding : "0px 3px"}}>{time}</span>
-                            <div style={{display : (this.state.hover ? "block" : "none")}}>
-                                <div>Delete</div>
-                                <div>Read/Unread</div>
+                            <div style={{display : (this.state.hover ? "none" : "block")}}>
+                                <span style={{padding : "0px 3px"}}>{date}</span><span style={{padding : "0px 3px"}}>{time}</span>
+                            </div>
+                            <div style={{display : (this.state.hover ? "flex" : "none")}}>
+                                <div style={{padding : "0px 5px"}}>Delete</div>
+                                <div style={{padding : "0px 5px"}}>Read/Unread</div>
                             </div>
                         </div>
-
                     </div>
                 </Link>
             </div>
