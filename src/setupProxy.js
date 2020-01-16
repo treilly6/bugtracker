d5aad6a66a6478594e4248c13bb036f626444273
treilly6 <treilly6@binghamton.edu>
2020-01-16 16:01:49 -0500

    cleanup

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,6 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
+    console.log("PROXY THE REQUEST FOR AUTHLOGIN ROUTE");
     app.use(proxy('/authLogin', { target: 'http://localhost:5000/' }));
 };
